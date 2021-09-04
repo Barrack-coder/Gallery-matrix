29ddbbb00b89c261fb11057b53a4723e2f4c22e4
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-09-04 19:21:06 +0300

    deleted some fuctionality in model.py

--- matrix/models.py
+++ matrix/models.py
@@ -10,9 +10,10 @@ def __str__(self) -> str:
 
 
 class Photo(models.Model):
-        category = models.ForeignKey(Category, on_delete=models.SET_NULL)
-        description = models.CharField(max_length=500, null=False, blank=False)
+        category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+        image = models.ImageField(null=False, blank=False)
+        description = models.TextField()
         
 def __str__(self) -> str:
-	return self.name
+	return self.description
 
