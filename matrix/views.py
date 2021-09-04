54774a7627a5d41a639e14c0e76f390da5750203
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-09-04 16:02:38 +0300

    modified app and add files

--- matrix/views.py
+++ matrix/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def gallery(request):
+        return render(request, 'matrix/gallery.html')
+
+def viewPhotos(request,pk):
+        return render(request, 'matrix/photos.html')
+
+def addPhoto(request):
+        return render(request, 'matrix/add.html')
