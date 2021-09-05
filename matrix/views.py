314e88a9caf099fec04dd83d3699a35303e6b7d2
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-09-05 22:20:40 +0300

    add functionality for forms in add.html

--- matrix/views.py
+++ matrix/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category, Photo
 
 # Create your views here.
@@ -12,17 +12,29 @@ def gallery(request):
 
 def viewPhoto(request,pk):
         photos = Photo.objects.get(id=pk)
-        return render(request, 'photos/photo.html', {'photo': Photo})
+        return render(request, 'photos/photo.html', {'photos': photos})
 
 def addPhoto(request):
         categories = Category.objects.all()
         
         if request.method == 'POST':
                 data = request.POST
-                image = request.FILE.get('image')
+                image = request.FILES.get('image')
                 
-                print('data:', data)
-                print('data:', image)
+        if data['category'] != 'none':
+                category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+                category, created = Category.objects.get_or_create(name=data['category_new'])
+        else:
+                category = None 
+                
+                photo = Photo.objects.create(
+                        category=category,
+                        description=data['description'],
+                        image=image,
+                ) 
+                return redirect('gallery') 
           
         context = {'categories': categories}  
         return render(request, 'photos/add.html', context)
+        
