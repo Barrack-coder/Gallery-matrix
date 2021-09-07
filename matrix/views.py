8d1429f17c634640ebd67c55a086bbd7bfa2ab1b
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-09-07 09:04:10 +0300

    modified matrix

--- matrix/views.py
+++ matrix/views.py
@@ -5,17 +5,20 @@ from .models import Category, Photo
 
 def gallery(request):
         category =request.GET.get('category')
-        print('category:', category)
+        if category == None:
+             photos = Photo.objects.all()
+        else:
+                photos = Photo.objects.filter(category__name__contains=category)   
         
         categories = Category.objects.all()
-        photos = Photo.objects.all()
+       
         
-        context = {'categories': categories, 'photos': photos }
+        context = {'categories': categories, 'photos': photos}
         return render(request, 'photos/gallery.html', context)
 
 def viewPhoto(request,pk):
-        photos = Photo.objects.get(id=pk)
-        return render(request, 'photos/photo.html', {'photos': photos})
+        photo = Photo.objects.get(id=pk)
+        return render(request, 'photos/photo.html', {'photo': photo})
 
 def addPhoto(request):
         categories = Category.objects.all()
@@ -24,20 +27,21 @@ def addPhoto(request):
                 data = request.POST
                 image = request.FILES.get('image')
                 
-        if data['category'] != 'none':
-                category = Category.objects.get(id=data['category'])
-        elif data['category_new'] != '':
-                category, created = Category.objects.get_or_create(name=data['category_new'])
-        else:
-                category = None 
-                
+                if data['category'] != 'none':
+                        category = Category.objects.get(id=data['category'])
+                elif data['category_new'] != '':
+                        category, created = Category.objects.get_or_create(name=data['category_new'])
+                else:
+                        category = None 
+                        
                 photo = Photo.objects.create(
                         category=category,
                         description=data['description'],
                         image=image,
                 ) 
+                photo.save()
                 return redirect('gallery') 
-          
+        photo= Photo.objects.all() 
         context = {'categories': categories}  
-        return render(request, 'photos/add.html', context)
+        return render(request, 'photos/add.html', context,{'photo': photo})
         
