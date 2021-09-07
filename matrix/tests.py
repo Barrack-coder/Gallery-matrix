6f40aa7e6b357cbde4c7c858e7a8e18081325e53
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-09-07 09:55:24 +0300

    modified readme

--- matrix/tests.py
+++ matrix/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from .models import Photo, Category
 # Create your tests here.
+
+class CategoryTestCase(TestCase):
+    
+    #Set up method to test
+    def setUp(self):
+        self.name = Category(name = 'Photo')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Photo,Category))
